5992a99bec62f6d42d9f7ab2e800a4473d9f8699
MrVictor <MrVictor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-15 16:10:43 +0000

    added 1.0 tag
    git-svn-id: https://plugins.svn.wordpress.org/custom-google-talk-chatback/tags/1.0@385119 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ custom-google-talk-chatback.php
@@ -0,0 +1,240 @@
+<?php
+/*
+Plugin Name: Custom Google Talk Chatback
+Plugin URI: http://intervaro.se/wordpress-google-talk-plugin
+Description: This plugin makes it easy to add a link to chat with you via Google Talk Chatback. Supports Widgets and Shortcodes atm.
+Author: Intervaro Web Agency in Lund, Sweden
+Version: 1.0
+Requires at least: 2.8
+Author URI: http://intervaro.se
+License: GPL
+*/
+
+require_once plugin_dir_path(__FILE__).'class-google-talk-status.php';
+
+// load translation files, if exists
+function myplugin_load_translation_file() {
+    $plugin_path = plugin_basename( dirname( __FILE__ ) .'/translations' );
+    load_plugin_textdomain( 'cgtc', '', $plugin_path );
+}
+add_action( 'init', 'cgtc_load_translation_file' );
+
+
+/****************************************************
+ * Widget Support
+ ****************************************************/
+
+class GoogleTalkWidget extends WP_Widget {
+
+	function GoogleTalkWidget() {
+		$widget_ops = array( 'classname' => 'widget_gtalk', 'description' => __('Google Talk Chatback', 'cgtc' ) );
+		$control_ops = array( 'width' => 400, 'height' => 350 );
+		$this->WP_Widget( 'gtalk', __( 'Google Talk', 'cgtc' ), $widget_ops, $control_ops );
+	}
+
+	// Form
+	function form( $instance ) {
+		$title					= esc_attr( $instance['title'] );
+		$hash						= esc_attr( $instance['hash'] );
+		$online					= esc_attr( $instance['online'] );
+		$online_link		= esc_attr( $instance['online_link'] );
+		$offline				= esc_attr( $instance['offline'] );
+		?>
+
+		<h3><?php _e('Settings', 'cgtc' ); ?></h3>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'cgtc' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('hash'); ?>"><?php _e('Hash:', 'cgtc' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'hash' ); ?>" name="<?php echo $this->get_field_name('hash'); ?>" type="text" value="<?php echo $hash; ?>" />
+			<?php _e('<small>Grab the hash code between <strong>tk=</strong> and <strong>&</strong> at:<br />', 'cgtc' ); ?>
+			http://www.google.com/talk/service/badge/New<br />
+			http://www.google.com/talk/service/a/<strong>yourdomain</strong>/badge/New (Domain users)</small>
+		</p>
+
+		<h3><?php _e('Online Output', 'cgtc' ); ?></h3>
+		<p>
+			<label for="<?php echo $this->get_field_id('online_'); ?>"><?php _e('Content (before link):', 'cgtc' ); ?></label>
+			<textarea class="widefat" id="<?php echo $this->get_field_id( 'online' ); ?>" name="<?php echo $this->get_field_name('online'); ?>" type="text"><?php echo $online; ?></textarea>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('online_link'); ?>"><?php _e('Link text or image:', 'cgtc' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('online_link'); ?>" name="<?php echo $this->get_field_name('online_link'); ?>" type="text" value="<?php echo $online_link; ?>" />
+		</p>
+
+
+		<h3><?php _e('Offline Output', 'cgtc' ); ?></h3>
+		<p>
+			<label for="<?php echo $this->get_field_id('offline'); ?>"><?php _e('Content:', 'cgtc' ); ?></label>
+			<textarea class="widefat" id="<?php echo $this->get_field_id('offline'); ?>" name="<?php echo $this->get_field_name('offline'); ?>" type="text"><?php echo $offline; ?></textarea>
+		</p>
+		
+		<?php
+	}
+
+	// Update
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['hash'] = strip_tags( $new_instance['hash'] );
+		$instance['online_link'] = $new_instance['online_link'];
+		$instance['online'] = $new_instance['online'];
+		$instance['offline'] = $new_instance['offline'];
+		return $instance;
+	}
+
+
+	// Widget
+	function widget( $args, $instance ) {
+		extract( $args );
+		$title				= apply_filters( 'widget_title', $instance['title'] );
+		$hash					= empty($instance['hash']) ? __('No hash supplied', 'cgtc' ) : $instance['hash'];
+		$online_link	= empty($instance['online_link']) ? __('Start chat', 'cgtc' ) : $instance['online_link'];
+		$online				= empty($instance['online']) ? __("I'm online.", 'cgtc' ) : $instance['online'];
+		$offline			= empty($instance['offline']) ? __("I'm offline.", 'cgtc' ) : $instance['offline'];
+
+		echo $before_widget;
+
+		if ($title) {
+			echo $before_title . $title . $after_title;
+		}
+
+		$gtalkStatus = new gtalkStatus($hash);
+		$status = ($gtalkStatus->isOnline()?'online':'offline');
+
+		if ( $status == 'online' ) {
+			?>
+			<div class="gtalk-message gtalk-online"><?php echo $online; ?></div>
+			<div class="gtalk-link-wrapper"><a class="gtalk-link" href="http://www.google.com/talk/service/badge/Start?tk=<?php echo $hash; ?>" onclick="window.open(\'http://www.google.com/talk/service/badge/Start?tk=<?php echo $hash; ?>\',\'popup\',\'width=250,height=450,scrollbars=no,resizable=no,toolbar=no,directories=no,location=no,menubar=no,status=no,left=50,top=50\'); return false"><?php echo $online_link; ?></a></div>
+			<?php
+		}
+		else {
+			?>
+			<div class="gtalk-message gtalk-offline"><?php echo $offline; ?></div>
+			<?php
+		}
+
+		echo $after_widget;
+	}
+}
+
+add_action( 'widgets_init', 'GoogleTalkWidgetInit' );
+
+function GoogleTalkWidgetInit() {
+	register_widget( 'GoogleTalkWidget' );
+}
+
+
+/****************************************************
+ * Shortcode Support
+ *
+ * [googletalk hash="" link="" offline=""]
+ * [gtalk_online] [/gtalk_online]
+ * [gtalk_offline] [/gtalk_offline]
+ *
+ ****************************************************/
+
+add_shortcode( 'gtalk', 'googletalk_handler' );
+add_shortcode( 'gtalk_online', 'googletalk_online_handler' );
+add_shortcode( 'gtalk_offline', 'googletalk_offline_handler' );
+
+// Google Talk Handler
+function googletalk_handler( $atts ) {
+	extract( shortcode_atts( array(
+		'hash' => '',
+		'link' => __('Start chat', 'cgtc' ),
+		'offline' => '',
+	), $atts ) );
+
+	$googletalk_output = '';
+	
+	if ( $hash ) {
+		$gtalkStatus = new gtalkStatus($hash);
+		$status = ($gtalkStatus->isOnline()?'online':'offline');
+		
+		if ( $status == 'online' ) {
+			$googletalk_output = '<a class="gtalk-link" href="http://www.google.com/talk/service/badge/Start?tk='.$hash.'" onclick="window.open(\'http://www.google.com/talk/service/badge/Start?tk='.$hash.'\',\'popup\',\'width=250,height=450,scrollbars=no,resizable=no,toolbar=no,directories=no,location=no,menubar=no,status=no,left=50,top=50\'); return false">'.$link.'</a>';
+		}
+		if ( $status == 'offline' ) {
+			$googletalk_output = '<span class="gtalk-message gtalk-offline">'.$offline.'</span>';
+		}
+	}
+	
+	return $googletalk_output;
+}
+
+// Google Talk Online Handler
+function googletalk_online_handler( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+		'hash' => ''
+	), $atts ) );
+
+	$googletalk_output = '';
+
+	if ( $hash ) {
+		$gtalkStatus = new gtalkStatus($hash);
+		$status = ($gtalkStatus->isOnline()?'online':'offline');
+		
+		if ( $status == 'online' ) {
+			$googletalk_output = do_shortcode($content);
+		}
+	}
+	
+	return $googletalk_output;
+}
+
+// Google Talk Offline Handler
+function googletalk_offline_handler( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+		'hash' => ''
+	), $atts ) );
+
+	$googletalk_output = '';
+
+	if ( $hash ) {
+		$gtalkStatus = new gtalkStatus($hash);
+		$status = ($gtalkStatus->isOnline()?'online':'offline');
+		
+		if ( $status == 'offline' ) {
+			$googletalk_output = do_shortcode($content);
+		}
+	}
+	
+	return $googletalk_output;
+}
+
+
+/****************************************************
+ * Template Tag Support
+ *
+ * http://andrewnacin.com/2010/05/18/rethinking-template-tags-in-plugins/
+ *
+ ****************************************************/
+
+
+// Return Google Talk Status
+function gtalk_status( $hash ) {
+	if ( $hash ) {
+		$gtalkStatus = new gtalkStatus($hash);
+		$status = ($gtalkStatus->isOnline()?'online':'offline');
+		if ( $status == 'online' ) { return true; }
+		if ( $status == 'offline' ) { return false; }
+	}
+	return false;
+}
+
+// Get Google Talk Link
+function gtalk_link( $hash, $link ) {
+	if ( $hash && $link) {
+		$gtalkStatus = new gtalkStatus($hash);
+		$status = ($gtalkStatus->isOnline()?'online':'offline');
+		
+		if ( $status == 'online' ) {
+			echo '<a class="gtalk-link" href="http://www.google.com/talk/service/badge/Start?tk='.$hash.'" onclick="window.open(\'http://www.google.com/talk/service/badge/Start?tk='.$hash.'\',\'popup\',\'width=250,height=450,scrollbars=no,resizable=no,toolbar=no,directories=no,location=no,menubar=no,status=no,left=50,top=50\'); return false">'.$link.'</a>';
+		}
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,85 @@
+=== Custom Google Talk Chatback ===
+Contributors: MrVictor
+Donate link: http://intervaro.se
+Tags: google, google talk, google talk chatback, gtalk, jabber, chat, instant message, support, online support, live support, live help, online chat, web chat, live chat, contact, contact form, widget, shortcode, template tag, plugin, links, sidebar
+Requires at least: 2.5
+Tested up to: 3.1
+Stable tag: 1.0
+
+Easily embed Goole Talk Chatback on your site for online chat support. Widget, shortcode or template tag support!
+
+== Description ==
+
+Embed links to your Google Talk Chatback. Display different things when online or offline. The plugin is made to be highly customizable.
+
+###Features
+
+* Custom "start chat link" and "offline text". Use text or image.
+* Display things depending on if the user is online or offline
+* Widget, Shortcode and Template Tag support
+* Translatable (send them to us if you make any)
+
+
+###Contact
+
+Go to the [Plugin Home Page over at Intervaro Web Agency](http://intervaro.se/custom-google-talk-chatback-wordpress-plugin) to give feedback or propose a feature!
+
+Special thanks to [Israelwebdev](http://israelwebdev.wordpress.com/2009/02/05/google-talk-status-api-in-php) who made the script that makes it possible to check if a user is online or offline.
+
+== Installation ==
+
+1. Upload `custom-google-talk-chatback` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Add the widget, insert shortcode into the WYSIWYG or use template tag in your theme template.
+
+###The Google Talk Hash Key (ID)
+
+To use this plugin you first need to know what you hash key is for your Google Talk account. The hash key is the string containing characters and numbers, found between the *tk=* and *&* at http://www.google.com/talk/service/badge/New or for domain users, at: http://www.google.com/talk/service/a/**YOUR-DOMAIN-HERE**/badge/New
+
+
+###Widget instructions
+
+Just drag the widget to the prefered sidebar and set it up as wanted.
+
+###Shortcode instructions
+
+There are 3 shortcodes available.
+
+[gtalk hash="" link="" offline=""]
+*Prints a link to start chat or a text message if offline*
+
+* Hash: your hash key (ID) for Google Talk Chatback
+* Link: link text
+* Offline (optional): text if offline
+
+[gtalk_online hash=""] [/gtalk_online]
+*Content inside shortcode will only be displayed if user is online*
+
+[gtalk_offline hash=""] [/gtalk_offline]
+*Content inside shortcode will only be displayed if user is offline*
+
+###Template Tag instructions
+
+`<?php gtalk_status( $hash ) ?>`
+
+*Returns 1 (true) if online and 0 (false) if offline*
+
+`<?php gtalk_link( $hash, $link ) ?>`
+
+*Returns a link to start chat*
+
+* Hash: your hash key (ID) for Google Talk Chatback
+* Link: link text or image (HTML)
+
+== Frequently Asked Questions ==
+
+None yet.. Have one? Visit the [Custom Google Talk Plugin Homepage](http://intervaro.se/custom-google-talk-chatback-wordpress-plugin) 
+
+== Screenshots ==
+
+1. Widget settings
+2. Widget example
+
+== Changelog ==
+
+== Upgrade Notice ==
